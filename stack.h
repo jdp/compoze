26124da8e9a64c7f832f16d1d425398be0a2c9cd
jdp <jdp34@njit.edu>
2009-12-12 04:29:05 -0500

    cleaned up stack.h

--- stack.h
+++ stack.h
@@ -11,10 +11,19 @@ typedef struct cz_stack
 #define czS_peek(s) (s->items[s->top])
 #define czS_empty(s) (s->top == 0)
 
-cz_stack * czS_create(int);
-int        czS_destroy(cz_stack *);
-int        czS_reset(cz_stack *s);
-int        czS_push(cz_stack *, cz_node *);
-cz_node *  czS_pop(cz_stack *);
+cz_stack *
+czS_create(int);
+
+int
+czS_destroy(cz_stack *);
+
+int
+czS_reset(cz_stack *s);
+
+int
+czS_push(cz_stack *, cz_node *);
+
+cz_node *
+czS_pop(cz_stack *);
 
 #endif
